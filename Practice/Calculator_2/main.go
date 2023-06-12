7756a209926aa9a6778ad2ad941ab08f5b8181c6
ICD96 <dead5476@gmail.com>
2023-06-12 21:56:14 +0600

    Не готово, нужно переделать.

--- Practice/Calculator_2/main.go
+++ Practice/Calculator_2/main.go
@@ -24,30 +24,34 @@ func main() {
 	var str, temp string
 	var glyph [8]string
 	j := 0
-	str = "12+14"
+	str = "1+2*3"
 	result := 0
 	for i := 0; i < len(str); i++ {
 		_, err = strconv.Atoi(str[i : i+1])
-		if err == nil { // Нужно дополнить проверкой на конец строки, иначе он просто не берет оставшееся число
+		if err == nil {
 			temp = temp + str[i:i+1]
 		} else {
 			num[j], _ = strconv.Atoi(temp)
-			fmt.Println(num[j])
 			glyph[j] = str[i : i+1]
 			j++
 			temp = ""
 		}
 	}
+	num[j], _ = strconv.Atoi(temp)
 	for i := 0; i < j; i++ { // Нужно реализовать чтобы он делал опереции поочередно (сначала умножение потом остально и тд)
+		switch glyph[i] {
+		case "/":
+			num[i], num[i+1] = div(num[i], num[i+1]), 0 //Н
+		case "*":
+			num[i], num[i+1] = mul(num[i], num[i+1]), 0
+		}
+	}
+	for i := 0; i < j; i++ {
 		switch glyph[i] {
 		case "+":
 			result = result + sum(num[i], num[i+1])
 		case "-":
 			result = result + sub(num[i], num[i+1])
-		case "/":
-			result = result + div(num[i], num[i+1])
-		case "*":
-			result = result + mul(num[i], num[i+1])
 		}
 	}
 	fmt.Println(result)
