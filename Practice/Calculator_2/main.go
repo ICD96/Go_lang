8167cc4c22e0df13603b66468855e235289ed00d
ICD96 <dead5476@gmail.com>
2023-06-24 23:58:38 +0600

    Не закончено

--- Practice/Calculator_2/main.go
+++ Practice/Calculator_2/main.go
@@ -19,40 +19,75 @@ func mul(a, b int) int {
 }
 
 func main() {
-	var num [8]int // Вспомнить как сделать массив без границ
+	str := "1+2*3+4" //Работает не со всеми условиями нужно будет попробовать 3*2+1*2 или 1+2*3+5
 	var err error
-	var str, temp string
-	var glyph [8]string
+	num := []int{}
+	glyph := []string{}
+	temp := ""
+	temp2 := 0
+	check := false
 	j := 0
-	str = "1+2*3"
-	result := 0
 	for i := 0; i < len(str); i++ {
 		_, err = strconv.Atoi(str[i : i+1])
-		if err == nil {
-			temp = temp + str[i:i+1]
-		} else {
-			num[j], _ = strconv.Atoi(temp)
-			glyph[j] = str[i : i+1]
-			j++
+		if err != nil {
+			if string(str[i]) == "*" || string(str[i]) == "/" {
+				glyph = append(glyph, string(str[i]))
+				temp2, _ = strconv.Atoi(temp)
+				num = append(num, temp2)
+				fmt.Println(num[j])
+				j++
+				check = true
+			} else {
+				check = false
+			}
 			temp = ""
+		} else {
+			temp = temp + string(str[i])
 		}
 	}
-	num[j], _ = strconv.Atoi(temp)
-	for i := 0; i < j; i++ { // Нужно реализовать чтобы он делал опереции поочередно (сначала умножение потом остально и тд)
+	if check {
+		temp2, _ = strconv.Atoi(temp)
+		num = append(num, temp2)
+		fmt.Println(num[j])
+		j++
+	}
+	check = false
+	temp = ""
+	for i := 0; i < len(str); i++ {
+		_, err = strconv.Atoi(str[i : i+1])
+		if err != nil {
+			if string(str[i]) == "+" || string(str[i]) == "-" {
+				glyph = append(glyph, string(str[i]))
+				temp2, _ = strconv.Atoi(temp)
+				num = append(num, temp2)
+				check = true
+				fmt.Println(num[j])
+				j++
+			} else {
+				check = false
+			}
+			temp = ""
+		} else {
+			temp = temp + string(str[i])
+		}
+	}
+	if check {
+		temp2, _ = strconv.Atoi(temp)
+		num = append(num, temp2)
+		fmt.Println(num[j])
+		j++
+	}
+	for i := 0; i < len(glyph); i++ {
 		switch glyph[i] {
 		case "/":
-			num[i], num[i+1] = div(num[i], num[i+1]), 0 //Н
+			num[i+1] = div(num[i], num[i+1])
 		case "*":
-			num[i], num[i+1] = mul(num[i], num[i+1]), 0
-		}
-	}
-	for i := 0; i < j; i++ {
-		switch glyph[i] {
+			num[i+1] = mul(num[i], num[i+1])
 		case "+":
-			result = result + sum(num[i], num[i+1])
+			num[i+1] = sum(num[i], num[i+1])
 		case "-":
-			result = result + sub(num[i], num[i+1])
+			num[i+1] = sub(num[i], num[i+1])
 		}
 	}
-	fmt.Println(result)
+	fmt.Println(num[len(num)-1])
 }
